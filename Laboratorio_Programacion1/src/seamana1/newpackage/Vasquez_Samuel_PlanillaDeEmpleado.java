753127b766f9ede902e9ea3409e7479f671fd079
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-04-25 14:19:13 -0600

    Primer ejercicio: PlanillaDelEmpleado
    ejercicio 1

---
+++ Laboratorio_Programacion1/src/seamana1/newpackage/Vasquez_Samuel_PlanillaDeEmpleado.java
@@ -0,0 +1,36 @@
+package seamana1.newpackage;
+import java.util.Scanner;
+// @author Samuel Josue Vasquez Fuentes
+
+public class Vasquez_Samuel_PlanillaDeEmpleado {
+    public static void main(String[] args){
+        Scanner enter = new Scanner(System.in);
+        enter.useDelimiter("\n");
+        float tPH,salr;
+        int hrMes;
+        String nom_Usuario; 
+  
+        System.out.println("Bienvenido a la Planilla Del Empleado");
+        System.out.println("Introduzca su Nombre");
+        nom_Usuario = enter.next();
+        
+        System.out.println("Introduzca sus Horas de Trabajo Mensuales");
+        hrMes = enter.nextInt();
+        System.out.println("Introduzca sus Tarifa por Hora");
+        tPH = enter.nextFloat();
+        
+            if(hrMes > 0 && tPH > 0){
+                salr = (hrMes*tPH)/4;
+                
+                System.out.println("------- Boleta del Empleado -------");
+                System.out.println("Nombre del Empleado: " + nom_Usuario);
+                System.out.println("Hora de Trabajo Mensual: " + hrMes);
+                System.out.println("Tarifa por Hora : " + tPH + " Lps.");
+                System.out.println("Salario del Empleado Semanal: " + salr + " Lps.");
+            }
+            else{
+                System.out.println("Porfavor introduzca numeros validos para realizar el calculo correctamente");
+            }
+        enter.close();
+    }
+}
