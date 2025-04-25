46b431df6e82f2107cd8e961a37023770ca3e412
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-04-25 16:58:23 -0600

    Create Vasquez_Samuel_Formulas.java
    Ejercicio 4

---
+++ Laboratorio_Programacion1/src/seamana1/newpackage/Vasquez_Samuel_Formulas.java
@@ -0,0 +1,145 @@
+package seamana1.newpackage;
+import java.util.Scanner;
+/**
+ *
+ * @author Samuel Vasquez 
+ */
+public class Vasquez_Samuel_Formulas {
+    public static void main(String[] args){ 
+        Scanner entrada = new Scanner(System.in);
+        float a, b, c, d, e, f, g, h, j;
+        float resp;
+     
+        System.out.println("Ingrese valor numero 1");
+        a = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 2");
+        b = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 3");
+        c = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 4");
+        d = entrada.nextFloat();
+        resp = (a/b)+(c/d);
+        System.out.println("Respuesta 1: " + resp);
+        
+        System.out.println("Ingrese valor numero 1");
+        a = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 2");
+        b = entrada.nextFloat();
+        resp = (1/(a-5))-((3*a*b)/4);
+        System.out.println("Respuesta 2: " + resp);
+        
+        System.out.println("Ingrese valor numero 1");
+        a = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 2");
+        b = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 3");
+        c = entrada.nextFloat();
+        resp = (a/b) + c;
+        System.out.println("Respuesta 3: " + resp);
+        
+        
+        System.out.println("Ingrese valor numero 1");
+        a = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 2");
+        b = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 3");
+        c = entrada.nextFloat();
+        resp = a + (b/c);
+        System.out.println("Respuesta 4: " + resp);
+        
+        System.out.println("Ingrese valor numero 1");
+        a = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 2");
+        b = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 3");
+        c = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 4");
+        d = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 5");
+        e = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 6");
+        f = entrada.nextFloat();
+        System.out.println("Valor de g");
+        g = entrada.nextFloat();
+        System.out.println("Valor de h");
+        h = entrada.nextFloat();
+        System.out.println("Valor de j");
+        j = entrada.nextFloat();
+        resp = ((a*a)/(b-c))+((d-e)/(f-((g*h)/j)));
+        System.out.println("Respuesta 5: " + resp);
+        
+        System.out.println("Ingrese valor numero 1");
+        a = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 2");
+        b = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 3");
+        c = entrada.nextFloat();
+        resp = (a/b) + c;
+        System.out.println("Respuesta 6: " + resp);
+        
+        System.out.println("Ingrese valor numero 1");
+        a = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 2");
+        b = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 3");
+        c = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 4");
+        d = entrada.nextFloat();
+        resp = a + (b/(c-d));
+        System.out.println("Respuesta 7: " + resp);
+        
+        System.out.println("Ingrese valor numero 1");
+        a = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 2");
+        b = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 3");
+        c = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 4");
+        d = entrada.nextFloat();
+        resp = ((a*a)/(b*b)) + ((c*c)/(d*d));
+        System.out.println("Respuesta 8: " + resp);
+        
+        System.out.println("Ingrese valor numero 1");
+        a = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 2");
+        b = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 3");
+        c = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 4");
+        d = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 5");
+        e = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 6");
+        f = entrada.nextFloat();
+        resp = (a-(b+c))/(d-(e/f));
+        System.out.println("Respuesta 9: " + resp);
+        
+        System.out.println("Ingrese valor numero 1");
+        a = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 2");
+        b = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 3");
+        c = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 4");
+        d = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 5");
+        e = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 6");
+        f = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 7");
+        g = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 8");
+        h = entrada.nextFloat();
+        resp = ((3*a + b)/(c-(d+5*e)))/((f+g)/(2*h));
+        System.out.println("Respuesta 9: " + resp);
+        
+        System.out.println("Ingrese valor numero 1");
+        a = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 2");
+        b = entrada.nextFloat();
+        System.out.println("Ingrese valor numero 3");
+        c = entrada.nextFloat();
+        resp = (a*a + 2*a*b + b*b)/((1/(c*c))+2);
+        System.out.println("Respuesta 10: " + resp);   
+    }
+}
