5acd67d227cf74421072905ed30e7eac69882232
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-05-02 15:39:53 -0600

    Ejercicio 2 Lab 2, Tiempo

---
+++ Laboratorio_Programacion1/src/semana2/Vasquez_Samuel_Tiempo.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package semana2;
+import java.util.Scanner;
+/**
+ *
+ * @author unwir
+ */
+public class Vasquez_Samuel_Tiempo {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        int sec;
+        
+        System.out.println("*****Bienvenido*****");
+        System.out.print("Introduzca la cantidad de segundos que quiere convertir(en numeros enteros): ");
+        sec = entrada.nextInt();
+        
+      
+        int h = (sec/3600);
+        int min = (sec % 3600)/60;
+        int res = sec % 60;
+        
+        String resul = (sec <=0) ? "Porfavor introduzca un numero mayor a 0" : "Su resultado seria " + String.format("%d", h) + " horas " + String.format("%d", min) + " minutos " + String.format("%d", res) + " segundos " ;
+        System.out.println(resul);
+        
+        
+    
+    
+    
+    }
+}
