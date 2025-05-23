127d68bf0252d12584cf75e34f4ef092d2e75dce
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-05-23 15:09:04 -0600

    piedra papel o tijera

--- Laboratorio_Programacion1/src/Semana5/Vasquez_Samuel_Examen1.java
+++ Laboratorio_Programacion1/src/Semana5/Vasquez_Samuel_Examen1.java
@@ -4,6 +4,7 @@
  */
 package Semana5;
 import java.util.Scanner;
+import java.util.Random;
 /**
  *
  * @author unwir
@@ -82,7 +83,58 @@ public class Vasquez_Samuel_Examen1 {
                     System.out.println("Descifrado: " + resultado2);
                 break;
                 case 3:
+                    Random compu = new Random();
+                    int opcCompu;
+                    boolean otra = true;
+                    String util;
+                    System.out.println("--- PIEDRA PAPEL O TIJERAS ---");
                     
+                    do{
+                        while(true){
+                            System.out.println("Introduzca que quiere usar (Piedra, Papel o Tijera): ");
+                            util = entrada.next();
+                            if(util.equalsIgnoreCase("Piedra") || util.equalsIgnoreCase("Papel") || util.equalsIgnoreCase("Tijera")){
+                                break;
+                            }else{
+                                System.out.println(">>>ERROR, porfavor introduzca una opcion valida");
+                            }
+                        }
+
+                        opcCompu = compu.nextInt(4);
+                        switch(opcCompu){
+                            case 1: 
+                                System.out.println("La computadora ha elegido PIEDRA"); break;
+                            case 2:
+                                System.out.println("La computadora ha elegido PAPEL"); break;
+                            case 3:
+                                System.out.println("La computadora ha elegido TIJERA"); break;
+                        }
+
+                        if((util.equalsIgnoreCase("Piedra") && opcCompu == 1)||(util.equalsIgnoreCase("Papel") && opcCompu == 2) || (util.equalsIgnoreCase("Tijera") && opcCompu == 3)){
+                            System.out.println("\n******HAS EMPATADO******");
+                        }else if((util.equalsIgnoreCase("Piedra") && opcCompu == 2)||(util.equalsIgnoreCase("Papel") && opcCompu == 3) || (util.equalsIgnoreCase("Tijera") && opcCompu == 1)){
+                            System.out.println("\n******HAS PERDIDO******");
+                        }else if((util.equalsIgnoreCase("Piedra") && opcCompu == 3)||(util.equalsIgnoreCase("Papel") && opcCompu == 1) || (util.equalsIgnoreCase("Tijera") && opcCompu == 2)){
+                            System.out.println("\n******HAS GANADO******");
+                        }
+                        
+                        while(true){
+                            System.out.print("\nQuiere jugar otra vez? (si/no): ");
+                            String devuelta = entrada.next();
+                            if(devuelta.equalsIgnoreCase("si")){
+                                otra = true;
+                                break;
+                            }else if(devuelta.equalsIgnoreCase("no")){
+                                otra = false;
+                                break;
+                            }else{
+                                System.out.println("\n>>> ERROR, porfavor solo indique si o no\n");
+                            }
+                        }
+                        
+                    }while(otra == true);
+                    
+                  break;
                 case 4:
                     
                 case 5: 
